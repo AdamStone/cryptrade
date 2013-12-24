8503f7f850a93445bd27e45a65619d52191cf8b4
AdamStone <arstone208@gmail.com>
2013-12-24 00:54:39 -0600

    plotting bugfixes

--- cryptrade/plotting.py
+++ cryptrade/plotting.py
@@ -5,12 +5,86 @@ from __future__ import division
 import numpy as np
 
 import matplotlib
-from matplotlib.finance import candlestick
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+# needed for customized candlestick from matplotlib.finance
+from matplotlib.lines import Line2D
+from matplotlib.patches import Rectangle
+
 from utilities import dt_to_ut, ut_to_dt, load_candlefile, parse_period
 
+
+def candlestick(ax, quotes, width=0.2, colorup='k', colordown='r', colorshadow=None,
+                alpha=1.0):
+
+    """
+    Modified from matplotlib.finance module. 
+    
+    quotes is a sequence of (time, open, close, high, low, ...) sequences.
+    As long as the first 5 elements are these values,
+    the record can be as long as you want (eg it may store volume).
+
+    time must be in float days format - see date2num
+
+    Plot the time, open, close, high, low as a vertical line ranging
+    from low to high.  Use a rectangular bar to represent the
+    open-close span.  If close >= open, use colorup to color the bar,
+    otherwise use colordown
+
+    ax          : an Axes instance to plot to
+    width       : fraction of a day for the rectangle width
+    colorup     : the color of the rectangle where close >= open
+    colordown   : the color of the rectangle where close <  open
+    alpha       : the rectangle alpha level
+
+    return value is lines, patches where lines is a list of lines
+    added and patches is a list of the rectangle patches added
+
+    """
+
+    OFFSET = width/2.0
+
+    lines = []
+    patches = []
+    for q in quotes:
+        t, open, close, high, low = q[:5]
+
+        if close>=open :
+            color = colorup
+            lower = open
+            height = close-open
+        else           :
+            color = colordown
+            lower = close
+            height = open-close
+	
+	if not colorshadow: colorsh = color
+	else: colorsh = colorshadow
+        vline = Line2D(
+            xdata=(t, t), ydata=(low, high),
+            color=colorsh,
+            linewidth=1,
+            antialiased=True,
+            alpha=0.5)
+
+        rect = Rectangle(
+            xy    = (t-OFFSET, lower),
+            width = width,
+            height = height,
+            facecolor = color,
+            edgecolor = color,
+            )
+        rect.set_alpha(alpha)
+
+
+        lines.append(vline)
+        patches.append(rect)
+        ax.add_line(vline)
+        ax.add_patch(rect)
+    ax.autoscale_view()
+
+    return lines, patches
 
 
 class Candleplot(object):
@@ -160,6 +234,6 @@ class Candleplot(object):
             self.ax1.legend(loc=2, fancybox=True, prop={'size':9})
     
         if plt.isinteractive():
-            plt.draw()
+            plt.pause(0.001)
         else:
             plt.show()
